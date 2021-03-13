99a088a2dc272668f4c0590f1280bb8534a984eb
abc7468 <abc7468@naver.com>
2021-03-13 23:56:57 +0900

    play

--- Algorithms/play.cpp
+++ Algorithms/play.cpp
@@ -1,53 +1,39 @@
-#include<queue>
-#include<iostream>
-#define MAX 1000
+#include <iostream>
 using namespace std;
-int sX, sY;
-int W, H;
-int map[MAX][MAX];
-int visited[MAX][MAX];
-int dx[4] = { -1,0,1,0 };
-int dy[4] = { 0,-1,0,1 };
-queue<pair<int, int>>q;
 
-void bfs() {
-	visited[sY][sX] = 1;
-	q.push(make_pair(sX, sY));
-	while (!q.empty()) {
-		int nowX = q.front().first; int nowY = q.front().second;
-		q.pop();
-		for (int k = 0; k < 4; k++) {
-			int nextX = nowX + dx[k]; int nextY = nowY + dy[k];
-			if (nextX >= 0 && nextX < W && nextY >= 0 && nextY < H) {
-				if (visited[nextY][nextX] == 0 && map[nextY][nextX]==1) {
-					q.push(make_pair(nextX, nextY));
-					visited[nextY][nextX] = visited[nowY][nowX] + 1;
-				}
-			}
-		}
+int fibonacci(int n)
+{
+	if (n == 0)
+	{
+		return 0;
+	}
+	else if (n == 1)
+	{
+		return 1;
+	}
+	else
+	{
+		return fibonacci(n - 1) + fibonacci(n - 2);
 	}
 }
-
-
-int main() {
-	cin >> H >> W;
-	for (int i = 0; i < H; i++) {
-		for (int j = 0; j < W; j++) {
-			cin >> map[i][j];
-			if (map[i][j] == 2) {
-				sX = j;
-				sY = i;
-			}
-		}
+int main()
+{
+	int n;
+	int a;
+	int dp[2][41] = { 0, };
+	cin >> n;
+	dp[0][0] = 1;
+	dp[1][0] = 0;
+	dp[0][1] = 0;
+	dp[1][1] = 1;
+	for (int i = 2; i < 41; i++)
+	{
+		dp[0][i] = dp[0][i - 1] + dp[0][i - 2];
+		dp[1][i] = dp[1][i - 1] + dp[1][i - 2];
 	}
-	bfs();
-	for (int i = 0; i < H; i++) {
-		for (int j = 0; j < W; j++) {
-			if (visited[i][j] == 0 && map[i][j]==0) visited[i][j]=1;
-			else if (visited[i][j] == 0 && map[i][j] == 1) visited[i][j] = 0;
-			cout << visited[i][j]-1 << ' ';
-		}
-		cout << '\n';
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a;
+		cout << dp[0][a] << ' ' << dp[1][a] << '\n';
 	}
-	return 0;
 }
