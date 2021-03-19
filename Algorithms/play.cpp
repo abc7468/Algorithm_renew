f715f4f04534f529928cad0f429f96764893797b
abc7468 <abc7468@naver.com>
2021-03-19 18:39:01 +0900

    2020 KAKAO BLIND RECRUITMENT : 브루트포스

--- Algorithms/play.cpp
+++ Algorithms/play.cpp
@@ -1,34 +1,68 @@
-#include<iostream>
+#include <vector>
+#include <cstring>
+#include <iostream>
 using namespace std;
+//m : column
+//n : row
+int dx[4];
+int dy[4];
+bool visited[101][101];
+int count_;
+void dfs(vector<vector<int>> &map, int col, int row, int value, int m, int n) {
+	visited[col][row] = true;
+	count_++;
 
+	int nowCol = col; int nowRow = row;
+	for (int k = 0; k < 4; k++) {
+		int nextCol = nowCol + dy[k];
+		int nextRow = nowRow + dx[k];
+		if (visited[nextCol][nextRow] == false) {
+			if (nextCol >= 0 && nextCol < m && nextRow >= 0 && nextRow < n) {
+				if (map[nextCol][nextRow] == value) {
+					dfs(map, nextCol, nextRow, value,m,n);
+				}
+			}
+		}
+	}
+}
+
+vector<int> solution(int m, int n, vector<vector<int>> picture) {
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			memset(visited[i], false, n * sizeof(bool));
+		}
+	}
+	dx[0] = -1;
+	dx[1] = 0;
+	dx[2] = 1;
+	dx[3] = 0;
+	dy[0] = 0;
+	dy[1] = -1;
+	dy[2] = 0;
+	dy[3] = 1;
+	int number_of_area = 0;
+	int max_size_of_one_area = 0;
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			if (visited[i][j] == false && picture[i][j] != 0) {
+				count_ = 0;
+				number_of_area++;
+				dfs(picture, i, j, picture[i][j],m,n);
+				if (max_size_of_one_area < count_)max_size_of_one_area = count_;
+			}
+		}
+	}
+
+
+
+
+
+	vector<int> answer(2);
+	answer[0] = number_of_area;
+	answer[1] = max_size_of_one_area;
+	return answer;
+}
 int main() {
-	int n; cin >> n;
-	int cmp = 1000 - n;
-	int ans = 0;
-	if (cmp >= 500) {
-		ans++;
-		cmp -= 500;
-	}
-	while (cmp >= 100) {
-		cmp -= 100;
-		ans++;
-	}
-	while (cmp >= 50) {
-		cmp -= 50;
-		ans++;
-	}
-	while (cmp >= 10) {
-		cmp -= 10;
-		ans++;
-	}
-	while (cmp >= 5) {
-		cmp -= 5;
-		ans++;
-	}
-	while (cmp >= 1) {
-		cmp -= 1;
-		ans++;
-	}
-	cout << ans;
-	return 0;
+	vector<int>a = solution(6, 4, { {1, 1, 1, 0},{1, 2, 2, 0},{1, 0, 0, 1},{0, 0, 0, 1},{0, 0, 0, 3},{0, 0, 0, 3} });
+	cout << a[0] << ' ' << a[1];
 }
---
+++ Algorithms/문자열 압축.cpp
@@ -0,0 +1,76 @@
+#include <string>
+#include <vector>
+#include<iostream>
+using namespace std;
+void get(string &str, int start, int len, vector<char> &v) {
+	for (int i = start; i < start + len; i++) {
+		v.push_back(str[i]);
+	}
+}
+
+int solution(string s) {
+	int answer = 0;
+	string tmp;
+	int sSize = s.size();
+	answer = sSize;
+	for (int i = 1; i <= sSize / 2; i++) {
+		vector<char> v;
+		int count = 1;
+		tmp = "";
+		int p = 0;
+		int endPoint = sSize / i;
+		for (p; p < endPoint; p++) {
+			if (v.empty()) {
+				get(s, p*i, i, v);
+			}
+			
+			int check = 0;
+			for (int j = p * i + i; j < p * i + i + i; j++) {
+				if (s[j] == v[j%i]) {
+					check++;
+					if (check == i) {
+						count++;
+					}
+				}
+				else {
+					if (count != 1) {
+						string y = to_string(count);
+						for (int u = 0; u < y.size(); u++) {
+							tmp.push_back(y[u]);
+
+						}
+					}			
+					for (int m = 0; m < i; m++) {
+						tmp.push_back(v[m]);
+					}
+					v.clear();
+					count = 1;
+					break;
+				
+				}
+
+			}
+		}
+		if (endPoint*i != sSize) {
+			for (int j = endPoint * i; j < sSize; j++) {
+				tmp.push_back(s[j]);
+			}
+		}
+		if (answer > tmp.size())answer = tmp.size();
+	}
+
+
+
+
+	return answer;
+}
+
+int main() {
+	cout << solution("xxxxxxxxxxyyy") << '\n';
+
+	cout << solution("a") << '\n';
+	cout << solution("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx") << '\n';
+	cout << solution("abcabcabcabcdededededede") << '\n';
+	cout << solution("xababcdcdababcdcd") << '\n';
+
+}
