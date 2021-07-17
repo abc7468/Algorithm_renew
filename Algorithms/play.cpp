6ea3f83aa0079192e55bcbacbb4bc060787c697b
abc7468 <abc7468@naver.com>
2021-07-17 21:38:17 +0900

    놀이공원 : 이분탐색

--- Algorithms/play.cpp
+++ Algorithms/play.cpp
@@ -1,37 +1,21 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
 
-int map[1001];
+class Student {
+public:
+	int _age;
+	Student() {};
+	Student(int age) : _age(age) {}
+};
+
 int main() {
-	int N; cin >> N;
-	for (int i = 0; i < N; i++) {
-		cin >> map[i];
-	}
-	int start = 0;
-	int end = 0;
-	int max = 0;
-	while (1) {
-		if (end == N-1) {
-			if (map[end] - map[start] > max)max = map[end] - map[start];
-			break;
-		}
-		if (map[end] < map[end + 1]) {
-			end++;
-		}
-		else {
-			if (start == end) {
-				start++;
-				end++;
-				continue;
-			}
-			else {
-				if (map[end] - map[start] > max)max = map[end] - map[start];
-				start = end + 1;
-				end = start;
-			}
-			
-		}
-	}
-	cout << max;
-	return 0;
+	Student s = Student(21);
+	Student* p = &s;
+
+
+	cout << "Value로 속성 접근 : " << s._age << '\n';
+	cout << "Pointer 타입으로 속성 접근 : " << p->_age << '\n';
 }
+
+
